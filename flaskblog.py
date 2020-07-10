82ca0cd138b3d26e5edcb9a178204e942db50222
Eric Dang <edang830@gmail.com>
2020-07-10 01:27:11 +0000

    Add register route and secret key

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '9ab13cd77620a65a973c7e09d00653b4'
 
 posts = [
     {
@@ -27,6 +29,12 @@ def hello():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
 
 
 if __name__ == "__main__":
