5a8baddbd5ea5caaf5391e1b276921368db8329e
Eric Dang <edang830@gmail.com>
2020-07-10 00:15:49 +0000

    Give about page a title

--- flaskblog.py
+++ flaskblog.py
@@ -2,16 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Eric Dang',
+        'title': 'Blog Post 1',
+        'content': 'This is the first post',
+        'date_posted': 'July 9, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'This is the second post',
+        'date_posted': 'July 10, 2020'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == "__main__":
