08d5a939236a677cebc3018b6e8dba53086cc32e
Eric Dang <edang830@gmail.com>
2020-07-14 18:14:46 +0000

    Add package __init__.py file

---
+++ flaskblog/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '9ab13cd77620a65a973c7e09d00653b4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from flaskblog import routes  # noqa
