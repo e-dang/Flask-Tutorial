070f5582cb7dc28bf489d3dfd41299be27477332
Eric Dang <edang830@gmail.com>
2020-07-22 19:52:04 +0000

    Remove csrf token from form unittest

--- tests/unittests/posts/test_forms.py
+++ tests/unittests/posts/test_forms.py
@@ -1,10 +1,8 @@
-from flask_wtf.csrf import generate_csrf
-
 from flaskblog.posts import forms
 
 
 def test_post_form(req, post):
-    form = forms.PostForm(obj=post, csrf_token=generate_csrf())
+    form = forms.PostForm(obj=post)
 
     form.submit()
 
