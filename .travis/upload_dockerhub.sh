29db8a567c12e106374cc5fbc6fe1c9972b62e99
Eric Dang <edang830@gmail.com>
2020-07-23 23:59:07 +0000

    Add different targets depending on branch

--- .travis/upload_dockerhub.sh
+++ .travis/upload_dockerhub.sh
@@ -4,11 +4,14 @@ echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 
 if [[ "${TRAVIS_BRANCH}" = "master" ]]; then
     TAG="latest"
+    TARGET="production_heroku"
 elif [[ "${TRAVIS_BRANCH}" = "dev" ]]; then
     TAG="latest-dev"
+    TARGET="dev"
 else
     TAG="latest-feature"
+    TARGET="dev"
 fi
 
-docker build -f Dockerfile -t $DOCKER_USER/$DOCKER_REPO:$TAG --target production_heroku .
+docker build -f Dockerfile -t $DOCKER_USER/$DOCKER_REPO:$TAG --target $TARGET .
 docker push $DOCKER_USER/$DOCKER_REPO:$TAG
