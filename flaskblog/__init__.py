5a18520cd00ae0e163a83d04da110b8761671884
Eric Dang <edang830@gmail.com>
2020-07-24 19:34:57 +0000

    Add links to the sidebar

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flaskblog.config import configs
+from random import randint
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -33,6 +34,17 @@ def create_app(config_class=os.environ.get('MY_FLASK_APP_CONFIG', 'dev'), **kwar
     app.register_blueprint(posts)
     app.register_blueprint(main)
     app.register_blueprint(errors)
+
+    random_links = ['https://github.com/e-dang',
+                    'https://github.com/e-dang/Composite-Peptide-Macrocycle-Generator',
+                    'https://github.com/e-dang/ConfBusterPlusPlus',
+                    'https://github.com/e-dang/dotfiles',
+                    'https://github.com/e-dang/K-Means',
+                    'https://github.com/e-dang/K-Medoids',
+                    'https://www.nba.com/warriors/',
+                    'https://www.ucla.edu/',
+                    'https://www.jbc.org/content/293/49/19038.short']
+    app.jinja_env.globals['rand_link'] = lambda: random_links[randint(0, len(random_links) - 1)]
 
     with app.app_context():
         db.create_all()
