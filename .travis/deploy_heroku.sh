0e9cb7f580df32cc37e8c83345c17ba766aee45d
Eric Dang <edang830@gmail.com>
2020-07-24 01:05:13 +0000

    Add login to heroku registry

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -3,10 +3,9 @@
 # wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 # heroku plugins:install @heroku-cli/plugin-container-registry
 
-# echo "$HEROKU_PASS" | docker login --username=_ --password-stdin registry.heroku.com
 # heroku container:push web --app $HEROKU_APP_NAME --arg TARGET=production_heroku
 # heroku container:release web --app $HEROKU_APP_NAME
-docker ls -a
+docker image ls
 echo $DOCKER_USER/$DOCKER_REPO:$TAG
 
 if [[ "${TRAVIS_BRANCH}" = "master" ]]; then
@@ -20,5 +19,6 @@ else
     export TARGET="dev"
 fi
 
-docker tag $DOCKER_USER/$DOCKER_REPO:$TAG registry.heroku.com/edang-flaskblog/web
-docker push registry.heroku.com/edang-flaskblog/web
+echo "$HEROKU_PASS" | docker login --username=$HEROKU_USER --password-stdin registry.heroku.com
+docker tag $DOCKER_USER/$DOCKER_REPO:$TAG registry.heroku.com/$HEROKU_APP_NAME/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
