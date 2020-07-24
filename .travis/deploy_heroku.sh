99ef9444def0e03e1e593b2c098714c0fb748e97
Eric Dang <edang830@gmail.com>
2020-07-24 00:57:53 +0000

    Set tag and target env variables

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -6,5 +6,19 @@
 # echo "$HEROKU_PASS" | docker login --username=_ --password-stdin registry.heroku.com
 # heroku container:push web --app $HEROKU_APP_NAME --arg TARGET=production_heroku
 # heroku container:release web --app $HEROKU_APP_NAME
-docker tag $DOCKER_USER/$DOCKER_REPO registry.heroku.com/edang-flaskblog/web
+docker ls -a
+echo $DOCKER_USER/$DOCKER_REPO:$TAG
+
+if [[ "${TRAVIS_BRANCH}" = "master" ]]; then
+    export TAG="latest"
+    export TARGET="production_heroku"
+elif [[ "${TRAVIS_BRANCH}" = "dev" ]]; then
+    export TAG="latest-dev"
+    export TARGET="dev"
+else
+    export TAG="latest-feature"
+    export TARGET="dev"
+fi
+
+docker tag $DOCKER_USER/$DOCKER_REPO:$TAG registry.heroku.com/edang-flaskblog/web
 docker push registry.heroku.com/edang-flaskblog/web
