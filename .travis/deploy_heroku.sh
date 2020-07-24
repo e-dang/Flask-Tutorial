2acecf50b19f9542a96830199aecf4078b39fe0b
Eric Dang <edang830@gmail.com>
2020-07-24 01:12:52 +0000

    Release app to heroku after uploading image

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-# heroku plugins:install @heroku-cli/plugin-container-registry
-
-# heroku container:push web --app $HEROKU_APP_NAME --arg TARGET=production_heroku
-# heroku container:release web --app $HEROKU_APP_NAME
-docker image ls
-echo $DOCKER_USER/$DOCKER_REPO:$TAG
-
 if [[ "${TRAVIS_BRANCH}" = "master" ]]; then
     export TAG="latest"
     export TARGET="production_heroku"
@@ -19,6 +11,8 @@ else
     export TARGET="dev"
 fi
 
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 echo "$HEROKU_PASS" | docker login --username=$HEROKU_USER --password-stdin registry.heroku.com
 docker tag $DOCKER_USER/$DOCKER_REPO:$TAG registry.heroku.com/$HEROKU_APP_NAME/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web
+heroku container:release web --app $HEROKU_APP_NAME
