75e8bd5a5e4b404388b45ed77db2d4c9100be26a
Eric Dang <edang830@gmail.com>
2020-07-24 00:51:39 +0000

    Use docker to push to heroku registry

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install @heroku-cli/plugin-container-registry
+# wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+# heroku plugins:install @heroku-cli/plugin-container-registry
 
-echo "$HEROKU_PASS" | docker login --username=_ --password-stdin registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME --arg TARGET=production_heroku
-heroku container:release web --app $HEROKU_APP_NAME
+# echo "$HEROKU_PASS" | docker login --username=_ --password-stdin registry.heroku.com
+# heroku container:push web --app $HEROKU_APP_NAME --arg TARGET=production_heroku
+# heroku container:release web --app $HEROKU_APP_NAME
+docker tag $DOCKER_USER/$DOCKER_REPO registry.heroku.com/edang-flaskblog/web
+docker push registry.heroku.com/edang-flaskblog/web
